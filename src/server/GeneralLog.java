ec9d5abd90e211d17a9acd3d3bae80967a062afa
Jeffrey Arcand <jeffrey.arcand@ariselab.ca>
2012-04-01 14:36:57 -0400

    Turned on the console logging for the host server back on.

--- src/server/GeneralLog.java
+++ src/server/GeneralLog.java
@@ -21,7 +21,7 @@ public class GeneralLog {
 	String pathName2 = "logging/Log.txt";
 	File file;
 	
-	boolean printOut = false;
+	boolean printOut = true;
 	
 	public GeneralLog(){
 		File dir = new File(".");
